a836acaee6c7fddce89612b05ab4b1c5f19b646f
Poly Titova <poly_titova@mail.ru>
2020-08-03 16:21:39 +0300

    make header

---
+++ src/components/header/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './header.css';
+
+const listQuestions = ["Разминка", "Ворбьиные", "Лесные птицы", "Певчие птицы", "Хищные птицы", "Морские птицы",];
+
+const Header = ({ count, score }) => {
+  return (
+    <div className="header d-flex">
+      <div className="top-indent d-flex">
+        <h3 className="logo">Songbirds</h3>
+        <div className="score">Score:
+        <span>{' ' + score}</span>
+        </div>
+      </div>
+      <ul className="pagination">
+        {
+          listQuestions.map((category, key) => (
+            <HeaderItem
+              category={category}
+              key={key}
+              active={count === key ? 'active' : null}
+            />
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
+
+const HeaderItem = ({ category, active }) => (
+  <li className="page-item">
+    <span className={`link ${active}`}>{category}</span>
+  </li>
+)
