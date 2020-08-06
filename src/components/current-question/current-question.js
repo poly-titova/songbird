9dbb2d5dc305c3df5b118723dd4fbbdf98e2d83d
Poly Titova <poly_titova@mail.ru>
2020-08-06 14:52:44 +0300

    add change in current-question

--- src/components/current-question/current-question.js
+++ src/components/current-question/current-question.js
@@ -4,17 +4,18 @@ import './current-question.css';
 
 import defaultBird from './default-bird.jpg'
 
-import { state, defaultName, bird } from '../../data/constants'
+import { defaultName, bird } from '../../data/constants'
 
 export default class CurrentQuestion extends Component {
 
   render() {
+    const { userAction, answer } = this.props
     return (
       <div className="current-question jumbotron rounded">
-        <img className="bird-image" src={state.userAction === state.answer ? bird.image : defaultBird}></img>
+        <img className="bird-image" src={userAction === answer ? bird.image : defaultBird}></img>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
-            <h3 className="term">{state.userAction === state.answer ? bird.name : defaultName}</h3>
+            <h3 className="term">{userAction === answer ? bird.name : defaultName}</h3>
           </li>
           <li className="list-group-item">
             <audio src={bird.audio} controls="true" />
