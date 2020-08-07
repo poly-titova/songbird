25c6397b78b906af2d3ee97a2ca3efc75925a1e3
Poly Titova <poly_titova@mail.ru>
2020-08-07 17:51:42 +0300

    add colors when the click

--- src/components/answers/answers.js
+++ src/components/answers/answers.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 
 import './answers.css';
 
-import { allBirdsInStage } from '../../data/constants'
-
 export default class Answers extends Component {
 
   render() {
-    const { comparisonId } = this.props;
+    const { allBirdsInStage, click, status } = this.props;
     return (
       <ul className="item-list list-group">
         {allBirdsInStage.map((item) => {
           const { id } = item
           return (
-            <li className="list-group-item"
+            <li className={`list-group-item ${status}`}
               id={item.id}
-              onClick={() => comparisonId(id)}>
+              onClick={() => click(id)}>
               <span className="li-btn"></span>
               {item.name}
             </li>
