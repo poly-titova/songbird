3919090477b7f02168e5245d2822312dc808a11d
Poly Titova <poly_titova@mail.ru>
2020-08-07 17:45:56 +0300

    change constants

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,21 +1,8 @@
-import birdsData from './birds-data'
 
-const state = {
-  count: 0,
-  score: 0,
-  answer: null,
-  userAction: null
-}
 const listQuestions = ["Разминка", "Ворбьиные", "Лесные птицы", "Певчие птицы", "Хищные птицы", "Морские птицы",];
 const defaultName = '*****'
-const allBirdsInStage = birdsData[state.count]
-const bird = allBirdsInStage[Math.floor(Math.random() * Math.floor(allBirdsInStage.length))]
-state.answer = bird.id
 
 export {
-  // state,
   listQuestions,
-  defaultName,
-  allBirdsInStage,
-  bird
+  defaultName
 }
